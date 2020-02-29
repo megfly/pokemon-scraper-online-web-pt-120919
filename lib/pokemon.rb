9087e1c641baa0d210ccac195fdebf450dd7f376
Meghan Flynn <mflynn333@gmail.com>
2020-02-29 00:48:05 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -20,14 +20,7 @@ class Pokemon
   end
   
   def self.find(id, db)
-    sql = <<-SQL
-      SELECT * FROM pokemon 
-      WHERE id = ? 
-      LIMIT 1
-    SQL
-    
-    row = db.execute(sql, id)[0]
-    Pokemon.new(id: row[0], name: row[1], type: row[2], db: db)
+    db.execute("SELECT * FROM pokemon WHERE id = ?", [id].flatten)
   end 
   
   
