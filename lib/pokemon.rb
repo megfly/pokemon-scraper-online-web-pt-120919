6dd3731f9e75c4d779b512d15052581eba2e911d
Meghan Flynn <mflynn333@gmail.com>
2020-02-29 00:36:06 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,26 @@
 class Pokemon
+  
+  attr_accessor :id, :name, :type, :db 
+  
+  def initialize(name:, type:, id:, db:)
+    @name = name
+    @type = type
+    @id = id
+    @db = db
+  end
+  
+  def self.save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL 
+        INSERT INTO pokemon (name, type)
+        VALUES (?,?)
+      SQL
+
+      DB[:conn].execute(sql, self.name, self.type)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
+  end
+  
 end
