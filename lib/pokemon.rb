3ed2f069d0ad8f4cc7c2e4bb60d25ab8ef30b89d
Meghan Flynn <mflynn333@gmail.com>
2020-02-29 00:44:03 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -20,6 +20,13 @@ class Pokemon
   end
   
   def self.find 
+    sql = <<-SQL
+      SELECT * FROM pokemon 
+      WHERE id = ? 
+    SQL
+    
+    row = db.execute(sql, id)
+    Pokemon.new(name: id: row[0], name: row[1], type: row[2], db: db)
   end 
   
   
